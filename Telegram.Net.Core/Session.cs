fced58a5462ee713dbf2315f63be830b65cfacc4
steavy29 <steavy@ukr.net>
2016-10-24 16:32:02 +0300

    Moved FileSessionStore and FakeSessionStore to Tests project. One should implement its own session store mechanism.

--- Telegram.Net.Core/Session.cs
+++ Telegram.Net.Core/Session.cs
@@ -12,52 +12,6 @@ namespace Telegram.Net.Core
         Session Load();
     }
 
-    public class FileSessionStore : ISessionStore
-    {
-        private readonly string sessionFileName;
-
-        public FileSessionStore(string sessionFileName = "session.dat")
-        {
-            this.sessionFileName = sessionFileName;
-        }
-
-        public void Save(Session session)
-        {
-            using (var stream = new FileStream(sessionFileName, FileMode.OpenOrCreate))
-            {
-                var result = session.ToBytes();
-                stream.Write(result, 0, result.Length);
-            }
-        }
-
-        public Session Load()
-        {
-            if (!File.Exists(sessionFileName))
-                return null;
-
-            using (var stream = new FileStream(sessionFileName, FileMode.Open))
-            {
-                var buffer = new byte[2048];
-                stream.Read(buffer, 0, 2048);
-
-                return Session.FromBytes(buffer, this);
-            }
-        }
-    }
-
-    public class FakeSessionStore : ISessionStore
-    {
-        public void Save(Session session)
-        {
-
-        }
-
-        public Session Load()
-        {
-            return null;
-        }
-    }
-
     public class Session
     {
         public string serverAddress;
