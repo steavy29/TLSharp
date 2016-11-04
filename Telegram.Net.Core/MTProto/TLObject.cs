0b4944a85080889cf7e6dc0c782339a7a21e9b64
Pavel <pash.alexandrov@yandex.ru>
2016-11-04 06:23:54 +0600

    add enum FileType

--- Telegram.Net.Core/MTProto/TLObject.cs
+++ Telegram.Net.Core/MTProto/TLObject.cs
@@ -12481,7 +12481,21 @@ namespace Telegram.Net.Core.MTProto
         FilterMusic = 0x3751b49e,
         FilterChatPhotos = 0x3a20ecb8,
     }
-
+    
+    public enum FileType : uint
+    {
+        FileUnknown = 0xaa963b05,
+        FileJpeg = 0x7efe0e,
+        FileGif = 0xcae1aadf,
+        FilePng = 0xa4f63c0,
+        FilePdf = 0xae1e508d,
+        FileMp3 = 0x528a0677,
+        FileMov = 0x4b09ebbc,
+        FilePartial = 0x40bc6f52,
+        FileMp4 = 0xb3cea0e4 ,
+        FileWebp = 0x1081464c,
+    }
+    
     public enum RpcRequestError
     {
         None = 0,
